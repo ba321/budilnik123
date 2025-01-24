dbaa22f99ca1a5f312f1d792145a873e262ab1a7
aaa <artemhik1110@gmail.com>
2025-01-24 21:08:02 +0300

    first commit

---
+++ app/src/main/java/com/example/budilnik123/AlarmReceiver.java
@@ -0,0 +1,34 @@
+package com.example.budilnik123;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @RequiresApi(api = Build.VERSION_CODES.Q)
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(4000);
+
+        Toast.makeText(context, "Просыпайтесь !!!", Toast.LENGTH_LONG).show();
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+
+
+        ringtone.play();
+    }
+}
